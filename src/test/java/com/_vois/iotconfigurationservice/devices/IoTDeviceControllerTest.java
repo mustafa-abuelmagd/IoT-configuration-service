871da839ee8d2c58c22ac19b7f26e5fc26cfc7c2
mustafaabuelmagd <mustafa.2buelmagd@gmail.com>
2023-06-20 14:22:13 +0300

    updated controller tests. initial clean

--- src/test/java/com/_vois/iotconfigurationservice/devices/IoTDeviceControllerTest.java
+++ src/test/java/com/_vois/iotconfigurationservice/devices/IoTDeviceControllerTest.java
@@ -3,7 +3,6 @@ package com._vois.iotconfigurationservice.devices;
 import com._vois.iotconfigurationservice.IoTConfigurationServiceApplication;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -52,18 +51,8 @@ class IoTDeviceControllerTest {
 
     private ClientAndServer mockServer;
 
-    /**
-     * Method under test: {@link IoTDeviceController#all()}
-     */
     @Test
     void testAll() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceRepository repository = mock(IoTDeviceRepository.class);
         ArrayList<IoTDevice> ioTDeviceList = new ArrayList<>();
@@ -74,43 +63,15 @@ class IoTDeviceControllerTest {
         verify(repository).findAll();
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#all()}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
     void testAll2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "com._vois.iotconfigurationservice.devices.IoTDeviceService.getAll()" because "this.service" is null
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.all(IoTDeviceController.java:20)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         (new IoTDeviceController(null)).all();
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#all()}
-     */
     @Test
     void testAll3() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         ArrayList<IoTDevice> ioTDeviceList = new ArrayList<>();
@@ -244,46 +205,16 @@ class IoTDeviceControllerTest {
         assertEquals(null, response.getStatusCode());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#create(IoTDevice)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
     void testCreate() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.IllegalStateException: Provided data is not correct!
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceService.create(IoTDeviceService.java:25)
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.create(IoTDeviceController.java:28)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceController ioTDeviceController = new IoTDeviceController(
                 new IoTDeviceService(mock(IoTDeviceRepository.class)));
         ioTDeviceController.create(new IoTDevice("Status", 1, 1));
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#create(IoTDevice)}
-     */
     @Test
     void testCreate2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
@@ -294,18 +225,8 @@ class IoTDeviceControllerTest {
         verify(ioTDeviceService).create(Mockito.<IoTDevice>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#getOne(long)}
-     */
     @Test
     void testGetOne() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceRepository repository = mock(IoTDeviceRepository.class);
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
@@ -315,46 +236,16 @@ class IoTDeviceControllerTest {
         verify(repository, atLeast(1)).findById(Mockito.<Long>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#getOne(long)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
     void testGetOne2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.IllegalStateException: No device exists with this ID
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceService.getOne(IoTDeviceService.java:33)
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.getOne(IoTDeviceController.java:33)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceRepository repository = mock(IoTDeviceRepository.class);
         when(repository.findById(Mockito.<Long>any())).thenReturn(Optional.empty());
         (new IoTDeviceController(new IoTDeviceService(repository))).getOne(1L);
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#getOne(long)}
-     */
     @Test
     void testGetOne3() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
@@ -364,46 +255,16 @@ class IoTDeviceControllerTest {
         verify(ioTDeviceService).getOne(Mockito.<Long>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#updateOne(IoTDevice, long)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
     void testUpdateOne() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
-
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.IllegalStateException: Provided data update is not valid!
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceService.updateOne(IoTDeviceService.java:41)
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.updateOne(IoTDeviceController.java:38)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceController ioTDeviceController = new IoTDeviceController(
                 new IoTDeviceService(mock(IoTDeviceRepository.class)));
         ioTDeviceController.updateOne(new IoTDevice("Status", 1, 1), 1L);
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#updateOne(IoTDevice, long)}
-     */
     @Test
     void testUpdateOne2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
@@ -414,18 +275,8 @@ class IoTDeviceControllerTest {
         verify(ioTDeviceService).updateOne(Mockito.<IoTDevice>any(), Mockito.<Long>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#deleteOne(long)}
-     */
     @Test
     void testDeleteOne() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceRepository repository = mock(IoTDeviceRepository.class);
         doNothing().when(repository).deleteById(Mockito.<Long>any());
@@ -433,43 +284,15 @@ class IoTDeviceControllerTest {
         verify(repository).deleteById(Mockito.<Long>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#deleteOne(long)}
-     */
     @Test
-    @Disabled("TODO: Complete this test")
     void testDeleteOne2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "com._vois.iotconfigurationservice.devices.IoTDeviceService.deleteOne(java.lang.Long)" because "this.service" is null
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.deleteOne(IoTDeviceController.java:43)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         (new IoTDeviceController(null)).deleteOne(1L);
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#deleteOne(long)}
-     */
     @Test
     void testDeleteOne3() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         doNothing().when(ioTDeviceService).deleteOne(Mockito.<Long>any());
@@ -477,18 +300,8 @@ class IoTDeviceControllerTest {
         verify(ioTDeviceService).deleteOne(Mockito.<Long>any());
     }
 
-    /**
-     * Method under test: {@link IoTDeviceController#configureDevice(int)}
-     */
     @Test
     void testConfigureDevice() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceRepository repository = mock(IoTDeviceRepository.class);
         when(repository.findById(anyLong())).thenReturn(Optional.ofNullable(Mockito.<IoTDevice>any()));
@@ -498,13 +311,6 @@ class IoTDeviceControllerTest {
 
     @Test
     void testConfigureDevice2() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         ArrayList<IoTDevice> ioTDeviceList = new ArrayList<>();
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
@@ -521,36 +327,14 @@ class IoTDeviceControllerTest {
 
 
     @Test
-    @Disabled("TODO: Complete this test")
     void testConfigureDevice3() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
-        // TODO: Complete this test.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "com._vois.iotconfigurationservice.devices.IoTDeviceService.configureDevice(int)" because "this.service" is null
-        //       at com._vois.iotconfigurationservice.devices.IoTDeviceController.configureDevice(IoTDeviceController.java:48)
-        //   See https://diff.blue/R013 to resolve this issue.
 
         (new IoTDeviceController(null)).configureDevice(1);
     }
 
     @Test
     void testConfigureDevice4() {
-        //   Diffblue Cover was unable to write a Spring test,
-        //   so wrote a non-Spring test instead.
-        //   Reason: R013 No inputs found that don't throw a trivial exception.
-        //   Diffblue Cover tried to run the arrange/act section, but the method under
-        //   test threw
-        //   java.lang.NullPointerException: Cannot invoke "Object.getClass()" because "bean" is null
-        //   See https://diff.blue/R013 to resolve this issue.
 
         IoTDeviceService ioTDeviceService = mock(IoTDeviceService.class);
         IoTDevice ioTDevice = new IoTDevice("Status", 1, 1);
